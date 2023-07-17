070c3effda07b9f262372973eba16a8561d7a384
Oscar Herrera <a.oscarherrera@gmail.com>
2023-07-17 18:09:00 -0500

    created a fully functioning rock paper scissors app

--- game.js
+++ game.js
@@ -1,17 +1,8 @@
 
-const getUsersInput = (input) => {
-    input = input.toLowercase();
-    if (input === 'scissor' || input === 'rock' || input === 'paper') {
-        return input
-    }
-    else {
-        console.log('error you must type rock paper or scissors!')
-    }
-};
+let computerScore = 0
+let playerScore = 0
 
-
-
-const getComputerChoice = () => {
+const getRandomChoice = () => {
     const randomNumber = Math.floor(Math.random()* 3);
         if (randomNumber === 0) {
     return 'rock'
@@ -29,3 +20,45 @@ const getComputerChoice = () => {
 };
 
 
+const computerSelection = getRandomChoice()
+
+function playRound (computerSelection,playerSelection) {
+    if (computerSelection === playerSelection) {
+        return 'the game is a tie'
+    }
+    if (computerSelection === 'rock') {
+        if (playerSelection === 'paper') {
+            return 'you won!'
+        } else {
+            return 'you lost'
+        }
+    }
+    if (computerSelection === 'paper') {
+        if (playerSelection === 'scissors') {
+            return 'you won!'
+        } else {
+            return 'you lost'
+        }
+    }
+    if (computerSelection === 'scissors') {
+        if (playerSelection === 'rock') {
+            return 'you won!'
+        } else {
+            return 'you lost'
+        }
+    }
+};
+
+
+const playerSelection =  prompt('type rock, paper, or scissors')
+
+const game = () => {
+    
+
+}
+
+console.log(game())
+
+
+console.log(computerSelection)
+console.log(playRound(computerSelection, playerSelection))
